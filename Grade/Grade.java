eaccc777628e2cc9ebbcb0e8c79a7204b42f3ed8
UnHumbleBen <31255679+UnHumbleBen@users.noreply.github.com>
2017-08-22 12:33:39 -0700

    Create Grade.java

---
+++ Grade/Grade.java
@@ -0,0 +1,38 @@
+// Benjamin Lee Period 5
+
+public class Grade {
+  
+  public static void main(String[] args) {
+  	
+    double GPA = 3.7;
+    
+    if (GPA < 1) {
+     	System.out.println("F"); 
+    } 
+    else if (GPA < 1.3) {
+     	System.out.println("D"); 
+    }
+	else if (GPA < 1.7) {
+     	System.out.println("D+"); 
+    }
+    else if (GPA < 2) {
+     	System.out.println("C-") 
+    }
+    else if (GPA < 2.7) {
+     	System.out.println("C") 
+    }
+    else if (GPA < 3) {
+     	System.out.println("C+") 
+    }
+    else if (GPA < 3.3) {
+     	System.out.println("D") 
+    }else if (GPA < 3.7) {
+     	System.out.println("D") 
+    }  
+    else if (GPA < 3.7) {
+    	System.out.println("A-")
+    }
+    
+    
+  }
+}
