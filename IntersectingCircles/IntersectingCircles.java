2b740027d9bbbaa00d8f7eb5dfb010b26ae4e547
UnHumbleBen <31255679+UnHumbleBen@users.noreply.github.com>
2017-08-29 11:43:20 -0700

    Update IntersectingCircles.java

--- IntersectingCircles/IntersectingCircles.java
+++ IntersectingCircles/IntersectingCircles.java
@@ -18,10 +18,11 @@ public class IntersectingCircles {
             int radius2 = input.nextInt();
             double distance = Math.sqrt(Math.pow(xCoor1 - xCoor2, 2) + Math.pow(yCoor1 - yCoor2, 2));
 
-            if (radius1 == radius2 && distance == 0) System.out.println("Intersect at infinitely many points (same circle)");
+            if (radius1 == radius2 && distance == 0 && radius1 != 0) System.out.println("Intersect at infinitely many points (same circle)");
+            else if (radius1 == radius2 && radius1 == 0 && distance == 0) System.out.println("Intersect at one point");
             else if (radius1 + radius2 == distance || Math.abs(radius1 - radius2) == distance) System.out.println("Intersect at one point");       
             else if (radius1 + radius2 >= distance && Math.abs(radius1 - radius2) <= distance) System.out.println("Intersect at two points");
-            else System.out.println("Do Not Intersect");
+            else System.out.println("Do not intersect");
 
             System.out.println("Press 1 to continue, Press 2 to stop");
             int user = input.nextInt();
