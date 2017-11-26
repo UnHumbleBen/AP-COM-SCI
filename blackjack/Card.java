7b17083fe3793be1e00ba948bcc47ff1cb2dfeff
UnHumbleBen <31255679+UnHumbleBen@users.noreply.github.com>
2017-11-25 23:40:51 -0800

    Update Card.java

--- blackjack/Card.java
+++ blackjack/Card.java
@@ -1,25 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package blackjack;
 
 public class Card {
-    private String suit;
-    private int value;
-    public Card (String newName, int newValue) {
+
+    private final String suit;
+    private final int value;
+    private final String name;
+
+    public Card(String newName, int newValue) {
         suit = newName;
         value = newValue;
+        String cardValue = value + "";
+        if (value == 1) {
+            cardValue = "Ace";
+        }
+        if (value == 0) {
+            cardValue = "King";
+        }
+        if (value == 11) {
+            cardValue = "Jack";
+        }
+        if (value == 12) {
+            cardValue = "Queen";
+        }
+        name = cardValue + " of " + suit;
+
     }
-    
+
     public String getName() {
+        /*
         String cardValue = value + "";
         if (value == 0) cardValue = "King";
         if (value == 11) cardValue = "Jack";
         if (value == 12) cardValue = "Queen";
         
         return cardValue + " of " + suit;
+*       */
+        return name;
     }
-    
+
+    public int getPoints() {
+        if (value == 1) {
+            return 11; // for aces
+        }
+        if (value == 0 || value >= 10) {
+            return 10; // for face cards
+        }
+        return value; // for normal cards
+    }
 }
