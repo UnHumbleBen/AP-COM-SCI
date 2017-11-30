3415559d4ccafd685138aa238205cd4a4c2b3530
UnHumbleBen <31255679+UnHumbleBen@users.noreply.github.com>
2017-11-30 11:52:12 -0800

    Update GameTester.java

--- blackjack/GameTester.java
+++ blackjack/GameTester.java
@@ -41,90 +41,91 @@ public class GameTester {
                 dealer.printHandWithoutFirstCardSoThatOtherPlayersCannotSeeItButTheDealerStillCanCauseTheRuleApparentlySaysSo();
                 System.out.println("Press 1 to continue");
                 int user1 = in.nextInt();
-                // hitting phase
+
+                // blackJack
+                boolean equal21 = false;
                 for (int i = 0; i < players.size(); i++) {
-                    System.out.println(players.get(i).getName() + "'s turn");
-                    while (true) {
-                        if (true) { // for players
-                            System.out.println("Your hand is: ");
-                            players.get(i).printHand();
-                            System.out.println("Your score is " + players.get(i).getScore());
+                    if (players.get(i).getScore() == 21) {
+                        equal21 = true;
+                        System.out.println("BLACKJACK!!! for " + players.get(i).getName());
+                        System.out.println("His hand was: ");
+                        players.get(i).printHand();
+                        System.out.println(players.get(i).getName()+ " is the winner!");
+                    }
+                }
 
-                            if (players.get(i).getScore() == 0) {
-                                System.out.println("You went over 21!");
-                                System.out.println("Busted!");
-                                System.out.println("Press 1 to continue");
-                                int user2 = in.nextInt();
-                                break;
+                // hitting phase
+                if (!equal21) {
+                    for (int i = 0; i < players.size(); i++) {
+                        System.out.println(players.get(i).getName() + "'s turn");
+                        while (true) {
+                            if (true) { // for players
+                                System.out.println("Your hand is: ");
+                                players.get(i).printHand();
+                                System.out.println("Your score is " + players.get(i).getScore());
+
+                                if (players.get(i).getScore() == 0) {
+                                    System.out.println("You went over 21!");
+                                    System.out.println("Busted!");
+                                    System.out.println("Press 1 to continue");
+                                    int user2 = in.nextInt();
+                                    break;
+                                }
+
+                                System.out.println("Press 1 to hit, Press 2 to pass");
+                                int user = in.nextInt();
+                                if (user != 1) {
+                                    break;
+                                }
+
+                                players.get(i).getCard(deck);
                             }
-
-                            System.out.println("Press 1 to hit, Press 2 to pass");
-                            int user = in.nextInt();
-                            if (user != 1) {
-                                break;
-                            }
-
-                            players.get(i).getCard(deck);
                         }
                     }
-                }
-                // scoring phase
-                int highest = players.get(0).getScore();
-                int index = 0;
-                for (int i = 1; i < players.size(); i++) {
-                    if (players.get(i).getScore() > highest) {
-                        highest = players.get(i).getScore();
-                        index = i;
+                    // finding highest score
+                    int highest = players.get(0).getScore();
+                    int index = 0;
+                    for (int i = 1; i < players.size(); i++) {
+                        if (players.get(i).getScore() > highest) {
+                            highest = players.get(i).getScore();
+                            index = i;
+                        }
                     }
-                }
 
-                //dealer hits
-                System.out.println("Dealer's Turn");
-                System.out.println("The Dealers hand is: ");
-                dealer.printHand();
-                System.out.println("His score is " + dealer.getScore());
-                while (dealer.getScore() < 17) {
-                    System.out.println("The Dealer hits");
-                    dealer.getCard(deck);
-
+                    //dealer hits
+                    System.out.println("Dealer's Turn");
                     System.out.println("The Dealers hand is: ");
                     dealer.printHand();
                     System.out.println("His score is " + dealer.getScore());
-                    
                     System.out.println("Press 1 to continue");
-                    int useless = in.nextInt();
-
-                    if (dealer.getScore() == 0) {
-                        System.out.println("Busted!!!");
-                        break;
-                    }
-                    /*
-                    System.out.println("Press 1 to continue");
-                    int user = in.nextInt();
-                    if (dealer.getScore() >= highest) {
-
-                        System.out.println("Dealer wins!");
-                        break;
-                    } else if (dealer.getScore() == 0) {
-                        System.out.println("Busted!!!");
-                        System.out.println(players.get(index).getName() + " IS THE WINNER");
-                        System.out.println("with a score of " + players.get(index).getScore());
-                        break;
-                    } else {
+                    int useless2 = in.nextInt();
+                    while (dealer.getScore() < 17) {
                         System.out.println("The Dealer hits");
                         dealer.getCard(deck);
-                    }
-                     */
-                }
-                System.out.println("Press 1 to see the winner");
-                int useless1 = in.nextInt();
 
-                if (dealer.getScore() >= highest) {
-                    System.out.println("Dealer wins!");
-                } else {
-                    System.out.println(players.get(index).getName() + " IS THE WINNER");
-                    System.out.println("with a score of " + players.get(index).getScore());
-                }
+                        System.out.println("The Dealers hand is: ");
+                        dealer.printHand();
+                        System.out.println("His score is " + dealer.getScore());
+
+                        System.out.println("Press 1 to continue");
+                        int useless = in.nextInt();
+
+                        if (dealer.getScore() == 0) {
+                            System.out.println("Busted!!!");
+                            break;
+                        }
+                    }
+                    // present the winner
+                    System.out.println("Press 1 to see the winner");
+                    int useless1 = in.nextInt();
+
+                    if (dealer.getScore() >= highest) {
+                        System.out.println("Dealer wins!");
+                    } else {
+                        System.out.println(players.get(index).getName() + " IS THE WINNER");
+                        System.out.println("with a score of " + players.get(index).getScore());
+                    }
+                } 
 
                 System.out.println("Press 1 to play another game, Press 2 to stop");
                 if (in.nextInt() != 1) {
